a31ce889a94e53bb2a7b9a28094825b6a8046340
ValVoy <v.cheron92@gmail.com>
2026-02-17 11:48:14 +0100

    Fix Unknown Action create in CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.new(
+      content: params[:content], 
+      gossip: @gossip, 
+      user: User.find_by(first_name: "Anonymous")
+    )
+
+    if @comment.save
+      flash[:success] = "Commentaire ajouté !"
+      redirect_to gossip_path(@gossip)
+    else
+      flash[:danger] = "Le commentaire ne peut pas être vide."
+      redirect_to gossip_path(@gossip)
+    end
+  end
 end
