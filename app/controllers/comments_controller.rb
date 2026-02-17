0e85b440e9e847d5426ef87081c4fce9d198d447
ValVoy <v.cheron92@gmail.com>
2026-02-17 12:26:22 +0100

    fixed issue with Destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,38 @@
 class CommentsController < ApplicationController
   def create
     @gossip = Gossip.find(params[:gossip_id])
-    
-    # On utilise l'association polymorphique ici
-    @comment = Comment.new(
-      content: params[:content], 
-      commentable: @gossip, # On remplace 'gossip' par 'commentable'
-      user: User.find_by(first_name: "Anonymous")
-    )
+    @comment = Comment.new(content: params[:content], commentable: @gossip, user: User.find_by(first_name: "Anonymous"))
 
     if @comment.save
       flash[:success] = "Commentaire ajouté !"
       redirect_to gossip_path(@gossip)
     else
-      flash[:danger] = "Erreur lors de l'ajout du commentaire."
+      flash[:danger] = "Erreur lors de l'ajout."
       redirect_to gossip_path(@gossip)
     end
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(content: params[:content])
+      flash[:success] = "Commentaire modifié !"
+      redirect_to gossip_path(@gossip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:success] = "Commentaire supprimé !"
+    redirect_to gossip_path(@gossip)
   end
 end
--- app/views/comments/comments_controller.rb
+++ app/views/comments/comments_controller.rb
@@ -1,17 +1,45 @@
 class CommentsController < ApplicationController
   def create
     @gossip = Gossip.find(params[:gossip_id])
-    @comment = Comment.new(content: params[:content], gossip: @gossip, user: User.find_by(first_name: "Anonymous"))
+    # Rappel : on utilise commentable car ta table est polymorphique
+    @comment = Comment.new(
+      content: params[:content], 
+      commentable: @gossip, 
+      user: User.find_by(first_name: "Anonymous")
+    )
 
     if @comment.save
       flash[:success] = "Commentaire ajouté !"
       redirect_to gossip_path(@gossip)
     else
-      # Si erreur, on recharge la page du potin avec un message
       flash[:danger] = "Erreur : le commentaire ne peut pas être vide."
       redirect_to gossip_path(@gossip)
     end
   end
 
-  # Les méthodes edit, update et destroy seront pour plus tard dans ton avancée
+  def edit
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    
+    if @comment.update(content: params[:content])
+      flash[:success] = "Commentaire mis à jour !"
+      redirect_to gossip_path(@gossip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:success] = "Commentaire supprimé !"
+    # On utilise status: :see_other pour aider Turbo à bien rediriger après un DELETE
+    redirect_to gossip_path(@gossip), status: :see_other
+  end
 end
